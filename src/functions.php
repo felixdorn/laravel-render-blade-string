011a0a5cd80473c6e0f612103505ab42761da6d3
Félix Dorn <github@felixdorn.fr>
2022-01-21 22:41:26 +0100

    php 8.1 support

--- src/functions.php
+++ src/functions.php
@@ -5,6 +5,7 @@ function __renderBlade(string $__php, array $__data = []): string
     $__data = array_merge([
         '__env' => app('view'),
     ], $__data);
+    /* @phpstan-ignore-next-line */
     $__php = app('blade.compiler')->compileString($__php);
 
     $obLevel = ob_get_level();
